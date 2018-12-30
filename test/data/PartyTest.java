4b742168914112027b9b9dcaabb618c5654720cc
gerard <ge_ml97@hotmail.com>
2018-12-30 19:05:42 +0100

    Test Party

---
+++ test/data/PartyTest.java
@@ -0,0 +1,35 @@
+package data;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PartyTest {
+    Party p, p1, psame;
+    @BeforeEach
+    void prepare(){
+        p = new Party("ABC");
+        p1 = new Party("DEF");
+        psame = new Party("ABC");
+    }
+    @Test
+    void getName() {
+        assertEquals("ABC", p.getName());
+        assertEquals("DEF", p1.getName());
+    }
+
+    @Test
+    void equalsDiferentObject() {
+        assertFalse(p.equals(p1));
+    }
+    @Test
+    void equalsDiferentObjectSamePartyName() {
+        assertTrue(p.equals(psame));
+    }
+    @Test
+    void toStringProperOutput(){
+        assertEquals("Party{name='ABC'}", p.toString());
+        assertEquals("Party{name='DEF'}", p1.toString());
+    }
+}
